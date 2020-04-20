5dc406078f10252e84ea52af5e9602dda72f4c67
michaelrejo1601 <ninjapenguin1601@gmail.com>
2020-04-20 01:49:33 -0700

    New Pics Added Fitting to Divs

--- functions.js
+++ functions.js
@@ -1,20 +1,22 @@
+var mobile = window.matchMedia("(max-width: 450px)").matches
 $(document).ready(function(){
   $(".down-arrow").click(function() {
     $([document.documentElement, document.body]).animate({
         scrollTop: $("#projects").offset().top
     }, {duration:1, easing:"linear"});
 });
-$(".project").click(function(event) {
-    var target = $(event.target);
+$(".project").click(function() {
     console.log(this);
     if (this.style.height == "30vh") {
-      this.style.height = "60vh";
+      this.style.height = "auto";
       this.style.marginTop = "calc(10vh + 10px)";
       this.style.boxShadow = "0px 4px 5px 1px grey";
+      this.style.overflow = "auto";
     } else {
       this.style.height = "30vh";
       this.style.marginTop = "10vh";
       this.style.boxShadow = "0px 10px 10px 1px grey";
+      this.style.overflow = "hidden";
     }
 });
 });
