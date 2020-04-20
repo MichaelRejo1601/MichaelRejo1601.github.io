23eaaa8bf1fd85749b82c3465496ca9e45ec3938
michaelrejo1601 <ninjapenguin1601@gmail.com>
2020-04-19 23:18:57 -0700

    Added Cards Expand w JS

--- functions.js
+++ functions.js
@@ -4,4 +4,17 @@ $(document).ready(function(){
         scrollTop: $("#projects").offset().top
     }, {duration:1, easing:"linear"});
 });
+$(".project").click(function(event) {
+    var target = $(event.target);
+    console.log(this);
+    if (this.style.height == "30vh") {
+      this.style.height = "60vh";
+      this.style.marginTop = "calc(10vh + 10px)";
+      this.style.boxShadow = "0px 4px 5px 1px grey";
+    } else {
+      this.style.height = "30vh";
+      this.style.marginTop = "10vh";
+      this.style.boxShadow = "0px 10px 10px 1px grey";
+    }
+});
 });
